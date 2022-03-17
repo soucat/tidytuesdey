b4e4d566da0d10f70cfbab464a2974edfdd304df
soucat <76273769+soucat@users.noreply.github.com>
2022-03-17 20:34:27 +0000

    :

--- script/charsets/iso-8859-2.py
+++ script/charsets/iso-8859-2.py
@@ -11,4 +11,18 @@
 #
 # Software distributed under the License is distributed on an "AS IS" basis,
 # WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limi
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Universal charset detector code.
+#
+# The Initial Developer of the Original Code is
+# Netscape Communications Corporation.
+# Portions created by the Initial Developer are Copyright (C) 2001
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#          Jehan <jehan@girinstud.io>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (
