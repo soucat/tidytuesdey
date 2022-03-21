99482d0c2b3bc71fd3fa91ab70b03b41128e3fe0
soucat <76273769+soucat@users.noreply.github.com>
2022-03-21 18:32:30 +0000

    Update gradle.properties

--- script/charsets/iso-8859-2.py
+++ script/charsets/iso-8859-2.py
@@ -46,4 +46,15 @@ aliases = ['ISO_8859-2:1987', 'ISO_8859-2', 'iso-ir-101',
 
 language = \
 {
-    'complete': [ 'bs', 'hr', '
+    'complete': [ 'bs', 'hr', 'cs', 'de', 'hu', 'pl', 'sr', 'sk', 'sl',
+                  'hsb', 'dsb', 'tk' ],
+    'incomplete': [ 'ro' ]
+}
+
+#   X0  X1  X2  X3  X4  X5  X6  X7  X8  X9  XA  XB  XC  XD  XE  XF   #
+charmap = \
+[
+    CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,RET,CTR,CTR,RET,CTR,CTR, # 0X
+    CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR, # 1X
+    SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM, # 2X
+    NUM,NUM,NUM,NUM,NUM,NU
