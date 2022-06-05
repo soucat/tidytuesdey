b5b84d0c55e80afb817f88add8e33b7add1d4c13
soucat <76273769+soucat@users.noreply.github.com>
2022-06-05 11:18:29 +0000

    cpufreq: interactive: sync freq feature for interactive governor

--- script/header-template.cpp
+++ script/header-template.cpp
@@ -30,4 +30,9 @@
  * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
  * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient m
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "../nsSBCharSetProber.h"
