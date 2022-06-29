26010dd263f7a30176d46e80bf516822d5f5a35b
soucat <76273769+soucat@users.noreply.github.com>
2022-06-29 19:44:00 +0000

    Not necessary

--- script/langs/tr.py
+++ script/langs/tr.py
@@ -30,4 +30,18 @@
 # in which case the provisions of the GPL or the LGPL are applicable instead
 # of those above. If you wish to allow use of your version of this file only
 # under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ##### END LICENSE BLOCK #####
+
+import re
+
+## Mandatory Properties ##
+
+# The human name for the language, in English.
+name = 'Turkish'
+# Use 2-letter ISO 639-1 if possi
