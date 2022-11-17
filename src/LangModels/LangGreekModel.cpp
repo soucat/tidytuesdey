8dbdedc87ef94d090bd9afa60ef4bde361670f6a
soucat <76273769+soucat@users.noreply.github.com>
2022-11-17 14:24:56 +0000

    -code cleanup

--- src/LangModels/LangGreekModel.cpp
+++ src/LangModels/LangGreekModel.cpp
@@ -96,4 +96,16 @@ static const unsigned char Iso_8859_7_CharToOrderMap[] =
   CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR,CTR, /* 9X */
   SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,SYM,ILL,SYM, /* AX */
   SYM,SYM,SYM,SYM,SYM,SYM, 17,SYM, 19, 22, 15,SYM, 16,SYM, 24, 28, /* BX */
-   55,  0, 25, 18, 20,  5, 29, 10, 26,  3,  8, 14, 13,  4, 3
+   55,  0, 25, 18, 20,  5, 29, 10, 26,  3,  8, 14, 13,  4, 31,  1, /* CX */
+   11,  6,ILL,  7,  2, 12, 27, 23, 45, 21, 51, 60, 17, 19, 22, 15, /* DX */
+   61,  0, 25, 18, 20,  5, 29, 10, 26,  3,  8, 14, 13,  4, 31,  1, /* EX */
+   11,  6,  9,  7,  2, 12, 27, 23, 45, 21, 51, 60, 16, 24, 28,ILL, /* FX */
+};
+/*X0  X1  X2  X3  X4  X5  X6  X7  X8  X9  XA  XB  XC  XD  XE  XF */
+
+
+/* Model Table:
+ * Total sequences: 1579
+ * First 512 sequences: 0.958419074626211
+ * Next 512 sequences (512-1024): 0.03968891876305471
+ * Rest: 0.001892006610734277
