6fd4f2d637758c6910c9885f81632c2dc35f9fa7
soucat <76273769+soucat@users.noreply.github.com>
2022-12-19 17:28:12 +0000

    Added credits.

--- src/nsEUCKRProber.h
+++ src/nsEUCKRProber.h
@@ -7,4 +7,14 @@
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
- * Software di
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All 
