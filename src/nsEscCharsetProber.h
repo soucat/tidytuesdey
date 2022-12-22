e1a4f8d338dc77749c2a505e3a8d8417a0eff3ff
soucat <76273769+soucat@users.noreply.github.com>
2022-12-22 21:46:58 +0000

    Code quality

--- src/nsEscCharsetProber.h
+++ src/nsEscCharsetProber.h
@@ -46,4 +46,17 @@
 class nsEscCharSetProber: public nsCharSetProber {
 public:
   nsEscCharSetProber(PRUint32 aLanguageFilter);
-  vir
+  virtual ~nsEscCharSetProber(void);
+  nsProbingState HandleData(const char* aBuf, PRUint32 aLen);
+  const char* GetCharSetName() {return mDetectedCharset;}
+  nsProbingState GetState(void) {return mState;}
+  void      Reset(void);
+  float     GetConfidence(void){return (float)0.99;}
+  void      SetOpion() {}
+
+protected:
+  void      GetDistribution(PRUint32 aCharLen, const char* aStr);
+  
+  nsCodingStateMachine* mCodingSM[NUM_OF_ESC_CHARSETS] ;
+  PRUint32    mActiveSM;
+  nsProbi
