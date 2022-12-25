c88529181378c4d12832c8e51e71845dac72b56f
soucat <76273769+soucat@users.noreply.github.com>
2022-12-25 11:42:19 +0000

    unused

--- src/nsHebrewProber.cpp
+++ src/nsHebrewProber.cpp
@@ -9,4 +9,16 @@
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language gov
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Universal charset detector code.
+ *
+ * The Initial Developer of the Original Code is
+ *          Shy Shalom <shooshX@gmail.com>
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used u
