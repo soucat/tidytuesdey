d8275d933d407f6328862dcc6ee69b23cc0ded81
soucat <76273769+soucat@users.noreply.github.com>
2022-12-30 13:01:44 +0000

    Update meetings.json

--- src/nsMBCSGroupProber.cpp
+++ src/nsMBCSGroupProber.cpp
@@ -15,4 +15,15 @@
  * The Original Code is Mozilla Universal charset detector code.
  *
  * The Initial Developer of the Original Code is
- * Netscape Commun
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *          Shy Shalom <shooshX@gmail.com>
+ *			Proofpoint, Inc.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which 
