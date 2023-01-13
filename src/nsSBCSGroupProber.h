146ad3460c27f727ae023164df25672959490feb
soucat <76273769+soucat@users.noreply.github.com>
2023-01-13 11:12:57 +0000

    Logo added

--- src/nsSBCSGroupProber.h
+++ src/nsSBCSGroupProber.h
@@ -7,4 +7,16 @@
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
- * Software distributed un
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Universal charset detector code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Con
