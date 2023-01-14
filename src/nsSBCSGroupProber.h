0a8bf6d7608e25d6434c81ce764663d144d1090b
soucat <76273769+soucat@users.noreply.github.com>
2023-01-14 11:36:16 +0000

    Update localizations

--- src/nsSBCSGroupProber.h
+++ src/nsSBCSGroupProber.h
@@ -37,4 +37,22 @@
  * ***** END LICENSE BLOCK ***** */
 
 #ifndef nsSBCSGroupProber_h__
-#
+#define nsSBCSGroupProber_h__
+
+
+#define NUM_OF_SBCS_PROBERS 35
+
+class nsCharSetProber;
+class nsSBCSGroupProber: public nsCharSetProber {
+public:
+  nsSBCSGroupProber();
+  virtual ~nsSBCSGroupProber();
+  nsProbingState HandleData(const char* aBuf, PRUint32 aLen);
+  const char* GetCharSetName();
+  nsProbingState GetState(void) {return mState;}
+  void      Reset(void);
+  float     GetConfidence(void);
+  void      SetOpion() {}
+
+#ifdef DEBUG_chardet
+  v
