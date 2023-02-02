08eb86c7e10e87e162ba87eb331e038f24790ba4
soucat <76273769+soucat@users.noreply.github.com>
2023-02-02 17:08:49 +0000

    Update guildwars.js

--- src/nsUTF8Prober.cpp
+++ src/nsUTF8Prober.cpp
@@ -27,3 +27,61 @@
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "nsUTF8Prober.h"
+
+void  nsUTF8Prober::Reset(void)
+{
+  mCodingSM->Reset(); 
+  mNumOfMBChar = 0;
+  mState = eDetecting;
+}
+
+nsProbingState nsUTF8Prober::HandleData(const char* aBuf, PRUint32 aLen)
+{
+  nsSMState codingState;
+
+  for (PRUint32 i = 0; i < aLen; i++)
+  {
+    codingState = mCodingSM->NextState(aBuf[i]);
+    if (codingState == eItsMe)
+    {
+      mState = eFoundIt;
+      break;
+    }
+    if (codingState == eStart)
+    {
+      if (mCodingSM->GetCurrentCharLen() >= 2)
+        mNumOfMBChar++;
+    }
+  }
+
+  if (mState == eDetecting)
+    if (GetConfidence() > SHORTCUT_THRESHOLD)
+      mState = eFoundIt;
+  return mState;
+}
+
+#define ONE_CHAR_PROB   (float)0.50
+
+float nsUTF8Prober::GetConfidence(void)
+{
+  float unlike = (float)0.99;
+
+  if (mNumOfMBChar < 6)
+  {
+    for (PRUint32 i = 0; i < mNumOfMBChar; i++)
+      unlike *= ONE_CHAR_PROB;
+    return (float)1.0 - unlike;
+  }
+  else
+    return (float)0.99;
+}
