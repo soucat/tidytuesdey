f97bd207d40bed9508c55ea03d6189470391d899
soucat <76273769+soucat@users.noreply.github.com>
2023-02-05 11:29:30 +0000

    Image optimization

--- src/nsUTF8Prober.h
+++ src/nsUTF8Prober.h
@@ -39,3 +39,16 @@
 #define nsUTF8Prober_h__
 
 #include "nsCharSetProber.h"
+#include "nsCodingStateMachine.h"
+
+class nsUTF8Prober: public nsCharSetProber {
+public:
+  nsUTF8Prober(){mNumOfMBChar = 0; 
+                mCodingSM = new nsCodingStateMachine(&UTF8SMModel);
+                Reset(); }
+  virtual ~nsUTF8Prober(){delete mCodingSM;}
+  nsProbingState HandleData(const char* aBuf, PRUint32 aLen);
+  const char* GetCharSetName() {return "UTF-8";}
+  nsProbingState GetState(void) {return mState;}
+  void      Reset(void);
+  float     GetConfiden
