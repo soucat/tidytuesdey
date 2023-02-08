70e621aac01cc7c5ec50c7ae4347c9a83e1557bc
soucat <76273769+soucat@users.noreply.github.com>
2023-02-08 07:30:24 +0000

    Add missing check

--- src/nsUniversalDetector.h
+++ src/nsUniversalDetector.h
@@ -17,4 +17,13 @@
  * The Initial Developer of the Original Code is
  * Netscape Communications Corporation.
  * Portions created by the Initial Developer are Copyright (C) 1998
- * the Initial Developer. All Righ
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others
