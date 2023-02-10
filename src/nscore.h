0bf0187e0f18fd71deafbaafe7d1563a85527298
soucat <76273769+soucat@users.noreply.github.com>
2023-02-10 18:59:07 +0000

    modify js

--- src/nscore.h
+++ src/nscore.h
@@ -27,4 +27,16 @@
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+#ifndef nsDummyCore_h__
+#define nsDummyCore_h__
+
+typedef bool PRBool;
+typedef int PRInt32;
+typedef unsigned int PRUint3
