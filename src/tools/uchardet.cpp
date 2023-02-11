b90ed76839892e2f68d1591397322c0c38cc5a2b
soucat <76273769+soucat@users.noreply.github.com>
2023-02-11 17:07:08 +0000

    reformat readme

--- src/tools/uchardet.cpp
+++ src/tools/uchardet.cpp
@@ -16,4 +16,13 @@
  * The Initial Developer of the Original Code is
  * Netscape Communications Corporation.
  * Portions created by the Initial Developer are Copyright (C) 2001
- * the Initial Develope
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *          BYVoid <byvoid.kcp@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file onl
