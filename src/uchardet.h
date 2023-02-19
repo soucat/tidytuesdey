2f278ef4e842253df2ae6ea56b11035754d5ac08
soucat <76273769+soucat@users.noreply.github.com>
2023-02-19 12:56:43 +0000

    Fixing build issues

--- src/uchardet.h
+++ src/uchardet.h
@@ -29,4 +29,24 @@
  * under the terms of either the GPL or the LGPL, and not to allow others to
  * use your version of this file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
- * and other provisions requ
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+#ifndef UCHARDET_H___
+#define UCHARDET_H___
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+
+typedef struct uchardet * uchardet_t;
+
+/**
+ * Create an encoding detector.
+ * @return a handle of a instance of uchardet
+ */
+u
